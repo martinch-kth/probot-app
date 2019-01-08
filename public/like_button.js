d04ddb5a6186330c137c9c54503086c35391e6d8
martinch-kth <martinch@kth.se>
2019-01-08 23:08:50 +0100

    ok

--- public/like_button.js
+++ public/like_button.js
@@ -1,4 +1,26 @@
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+ //   this.state = { liked: false };
+  }
+
+  render() {
+  //  if (this.state.liked) {
+      return 'You liked this.';
+  //  }
+
+    return e(
+      <p>hiii</p>
+ //     'button',
+ //     { onClick: () => this.setState({ liked: true }) },
+ //     'Like'
+    );
+  }
+}
+
+const domContainer = document.querySelector('#root');
+ReactDOM.render(e(LikeButton), domContainer);
